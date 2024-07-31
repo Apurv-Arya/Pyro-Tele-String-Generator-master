b1f08d6502d95d3b9c8a996ef7a3cc93f473a48b
Apurv Arya <apurvarya19@gmail.com>
2024-08-01 02:04:58 +0530

    updates

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
-from os import getenv
+import os
 
-API_ID = int(getenv("API_ID", "28611965"))
-API_HASH = getenv("API_HASH", "d36cfa9250dd3d0d46678b538836ca8b")
-BOT_TOKEN = getenv("BOT_TOKEN", "7270342784:AAHEzCyWZ4xyBpja1Z9ABBOhlKcCXL02jgs")
-DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://BinaryBandiT69:BinaryBandiT69@db9.cj2gfsp.mongodb.net/?retryWrites=true&w=majority&appName=DB9")
+API_ID = int(os.environ.get("API_ID", "28611965"))
+API_HASH = os.environ.get("API_HASH", "d36cfa9250dd3d0d46678b538836ca8b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7270342784:AAHEzCyWZ4xyBpja1Z9ABBOhlKcCXL02jgs")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://BinaryBandiT69:BinaryBandiT69@db9.cj2gfsp.mongodb.net/?retryWrites=true&w=majority&appName=DB9")
 MUST_JOIN = "TBots_Father"
